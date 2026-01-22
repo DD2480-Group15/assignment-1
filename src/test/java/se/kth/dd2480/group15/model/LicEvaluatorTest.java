e39c80812606d5347b652a9add1d0452bd7f2de0
RasmusSjo <rasmus.sjoberg15@gmail.com>
2026-01-22 12:18:45 +0100

    build: add placeholder tests for LIC methods, fixes #1

---
+++ src/test/java/se/kth/dd2480/group15/model/LicEvaluatorTest.java
@@ -0,0 +1,75 @@
+package se.kth.dd2480.group15.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class LicEvaluatorTest {
+
+    @Test
+    void lic0_validInput() {
+    }
+
+    @Test
+    void lic0_invalidInput() {
+    }
+
+    @Test
+    void lic1() {
+    }
+
+    @Test
+    void lic2() {
+    }
+
+    @Test
+    void lic3() {
+    }
+
+    @Test
+    void lic4() {
+    }
+
+    @Test
+    void lic5() {
+    }
+
+    @Test
+    void lic6() {
+    }
+
+    @Test
+    void lic7() {
+    }
+
+    @Test
+    void lic8() {
+    }
+
+    @Test
+    void lic9() {
+    }
+
+    @Test
+    void lic10() {
+    }
+
+    @Test
+    void lic11() {
+    }
+
+    @Test
+    void lic12() {
+    }
+
+    @Test
+    void lic13() {
+    }
+
+    @Test
+    void lic14() {
+    }
+
+    @Test
+    void evaluateLics() {
+    }
+}
