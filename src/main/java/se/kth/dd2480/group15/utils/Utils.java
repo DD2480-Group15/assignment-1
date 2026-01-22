c35abc926c8b1231f04728d5c77546c8a7c531f4
RasmusSjo <rasmus.sjoberg15@gmail.com>
2026-01-22 11:48:40 +0100

    feat: add utils class (no functionality yet), refs #1

---
+++ src/main/java/se/kth/dd2480/group15/utils/Utils.java
@@ -0,0 +1,7 @@
+package se.kth.dd2480.group15.utils;
+
+public class Utils {
+
+    // In this class, utility methods will be defined, such as checking
+    // if three points are inside/outside a circle, check their angle, etc
+}
