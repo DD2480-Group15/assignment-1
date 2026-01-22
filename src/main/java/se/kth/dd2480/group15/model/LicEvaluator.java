0ab86a6738ead6b68cb5a18c774e9ffa1853f786
RasmusSjo <rasmus.sjoberg15@gmail.com>
2026-01-22 11:49:21 +0100

    feat: add placeholders for LIC methods, fixes #1

--- src/main/java/se/kth/dd2480/group15/model/LicEvaluator.java
+++ src/main/java/se/kth/dd2480/group15/model/LicEvaluator.java
@@ -2,4 +2,83 @@ package se.kth.dd2480.group15.model;
 
 public class LicEvaluator {
 
+    public boolean Lic0() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic1() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic2() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic3() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic4() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic5() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic6() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic7() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic8() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic9() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic10() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic11() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic12() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic13() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean Lic14() {
+        // TODO Implement functionality
+        return false;
+    }
+
+    public boolean[] evaluateLics() {
+        // TODO Implement functionality
+        return new  boolean[0];
+    }
 }
