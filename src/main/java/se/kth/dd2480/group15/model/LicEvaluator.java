5a4d0f58ca369b9f696d327cf63f38e3c0bb0a4a
Selinaliu1030 <selinaliu1030@gmail.com>
2026-01-23 11:13:48 +0100

    feat: add lic5 implementation and corresponding tests #7

--- src/main/java/se/kth/dd2480/group15/model/LicEvaluator.java
+++ src/main/java/se/kth/dd2480/group15/model/LicEvaluator.java
@@ -29,8 +29,20 @@ public class LicEvaluator {
         return false;
     }
 
-    public boolean Lic5() {
-        // TODO Implement functionality
+    public boolean Lic5(int numpoints, Point[] pt) {
+        /* 
+        Input: n (integer, number of data points), 
+               pt (arrays of Point containing the x and y coordinates of the data points)
+
+        Output: boolean (true if the condition is met, false otherwise)       
+        
+        Functionality: There exists at least one set of two consecutive data points, (X[i],Y[i]) and (X[j],Y[j]), such
+        that X[j] - X[i] < 0. (where i = j-1)
+        */
+
+        for(int i=1; i<numpoints; i++)
+            if(pt[i].x() - pt[i-1].x() < 0)
+                return true;
         return false;
     }
 
